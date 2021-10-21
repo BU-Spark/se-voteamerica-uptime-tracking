2251b2d853a69b15a840d9efca481a1e511f23fd
Jeffrey Jin <jinjeff2@gmail.com>
2021-10-21 16:40:45 -0400

    connected table to api

--- src/components/HomeTable.vue
+++ src/components/HomeTable.vue
@@ -12,23 +12,23 @@
         <md-table-cell md-label="Status" md-sort-by="status">{{
           item.status
         }}</md-table-cell>
-        <md-table-cell md-label="Status Changed At" md-sort-by="statusChangedAt">{{
-          item.statusChangedAt
+        <md-table-cell md-label="Status Changed At" md-sort-by="status_changed_at">{{
+          item.status_changed_at
         }}</md-table-cell>
-        <md-table-cell md-label="Uptime Day" md-sort-by="uptimeDay">{{
-          item.uptimeDay
+        <md-table-cell md-label="Uptime Day" md-sort-by="uptime_day">{{
+          item.uptime_day
         }}</md-table-cell>
-        <md-table-cell md-label="Uptime Week" md-sort-by="upTimeWeek">{{
-          item.upTimeWeek
+        <md-table-cell md-label="Uptime Week" md-sort-by="uptime_week">{{
+          item.uptime_week
         }}</md-table-cell>
-        <md-table-cell md-label="Uptime Month" md-sort-by="uptimeMonth">{{
-          item.uptimeMonth
+        <md-table-cell md-label="Uptime Month" md-sort-by="uptime_month">{{
+          item.uptime_month
         }}</md-table-cell>
-        <md-table-cell md-label="Uptime Quarter" md-sort-by="upTimeQuarter">{{
-          item.upTimeQuarter
+        <md-table-cell md-label="Uptime Quarter" md-sort-by="uptime_quarter">{{
+          item.uptime_quarter
         }}</md-table-cell>
-        <md-table-cell md-label="Links" md-sort-by="links">{{
-          item.links
+        <md-table-cell md-label="Links" md-sort-by="url">{{
+          item.url
         }}</md-table-cell>
       </md-table-row>
     </md-table>
@@ -40,43 +40,48 @@ export default {
   data() {
     return {
         services:[{
-          id: 1,
-          description: 'VT Polling Place Lookup, Voter Registration Status Verifier, Absentee Ballot Request, and Absentee Ballot Tracker',
-          status: 'up',
-          statusChangedAt: '06/10/2021 2:39 a.m.',
-          uptimeDay: '99.00%',
-          upTimeWeek: '100.00%',
-          uptimeMonth: '100.00%',
-          upTimeQuarter: '100.00%',
-          links: "links"
-        },
-        {
-          id: 2,
-          description: 'IN Absentee Ballot Request',
-          status: 'up',
-          statusChangedAt: '06/20/2021 12:27 a.m.',
-          uptimeDay: '100.00%',
-          upTimeWeek: '100.00%',
-          uptimeMonth: '100.00%',
-          upTimeQuarter: '100.00%',
-          links: "links"
-        },
-        {
-          id: 3,
-          description: 'IN Polling Place Lookup, Online Voter Registration, Voter Registration Status Verifier, and Absentee Ballot Tracker',
-          status: 'up',
-          statusChangedAt: '07/02/2021 5:46 a.m.',
-          uptimeDay: '100.00%',
-          upTimeWeek: '100.00%',
-          uptimeMonth: '100.00%',
-          upTimeQuarter: '100.00%',
-          links: "links"
+        //   id: 1,
+        //   description: 'VT Polling Place Lookup, Voter Registration Status Verifier, Absentee Ballot Request, and Absentee Ballot Tracker',
+        //   status: 'up',
+        //   statusChangedAt: '06/10/2021 2:39 a.m.',
+        //   uptimeDay: '99.00%',
+        //   upTimeWeek: '100.00%',
+        //   uptimeMonth: '100.00%',
+        //   upTimeQuarter: '100.00%',
+        //   links: "links"
+        // },
+        // {
+        //   id: 2,
+        //   description: 'IN Absentee Ballot Request',
+        //   status: 'up',
+        //   statusChangedAt: '06/20/2021 12:27 a.m.',
+        //   uptimeDay: '100.00%',
+        //   upTimeWeek: '100.00%',
+        //   uptimeMonth: '100.00%',
+        //   upTimeQuarter: '100.00%',
+        //   links: "links"
+        // },
+        // {
+        //   id: 3,
+        //   description: 'IN Polling Place Lookup, Online Voter Registration, Voter Registration Status Verifier, and Absentee Ballot Tracker',
+        //   status: 'up',
+        //   statusChangedAt: '07/02/2021 5:46 a.m.',
+        //   uptimeDay: '100.00%',
+        //   upTimeWeek: '100.00%',
+        //   uptimeMonth: '100.00%',
+        //   upTimeQuarter: '100.00%',
+        //   links: "links"
         },]
     };
   },
   async mounted() {
-    //   const serviceInfo = await fetch("http://uptime.voteamerica.com/v1/uptime/proxies/");
-    //   this.services = serviceInfo.results;
+      // const serviceInfo = await fetch("http://vapre.us.to:9901/v1/uptime/sites/");
+      // this.services = serviceInfo;
+      // console.log(this.services.json());
+      const response = await fetch("http://vapre.us.to:9901/v1/uptime/sites/")
+        .then(results => { return results.json()});
+      this.services = response.results;
+      console.log(this.services);
   },
 };
 </script>
